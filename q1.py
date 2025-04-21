d008f1982f02f16c0691f3200047bda4f7a11b21
inbal-sh <inbalshmuel1@tau.ac.oil>
2025-04-21 13:31:04 +0300

    added content to q1.py and q2.py

---
+++ q1.py
@@ -0,0 +1,48 @@
+MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+              'D': '-..',    'E': '.',      'F': '..-.',
+              'G': '--.',    'H': '....',   'I': '..',
+              'J': '.---',   'K': '-.-',    'L': '.-..',
+              'M': '--',     'N': '-.',     'O': '---',
+              'P': '.--.',   'Q': '--.-',   'R': '.-.',
+              'S': '...',    'T': '-',      'U': '..-',
+              'V': '...-',   'W': '.--',    'X': '-..-',
+              'Y': '-.--',   'Z': '--..',
+              '0': '-----',  '1': '.----',  '2': '..---',
+              '3': '...--',  '4': '....-',  '5': '.....',
+              '6': '-....',  '7': '--...',  '8': '---..',
+              '9': '----.',
+              '.': '.-.-.-', ',': '--..--', ':': '---...',
+              "'": '.----.', '-': '-....-',
+              }
+def english_to_morse(
+    input_file: str = "lorem.txt",
+    output_file: str = "lorem_morse.txt"
+):
+    """Convert an input text file to an output Morse code file.
+    Notes
+    -----
+    This function assumes the existence of a MORSE_CODE dictionary, containing a
+    mapping between English letters and their corresponding Morse code.
+    Parameters
+    ----------
+    input_file : str
+        Path to file containing the text file to convert.
+    output_file : str
+        Name of output file containing the translated Morse code. Please don't change
+        it since it's also hard-coded in the tests file.
+    """
+    # Read the file as a single text
+    with open(input_file, 'r', encoding='utf-8') as f_in:
+        text = f_in.read()
+    
+    # Convert all letters to uppercase
+    text = text.upper()
+    
+    # Split the text into words and convert each word to Morse code
+    # Each Morse word will be placed on a new line in the output file
+    morse_text = '\n'.join([' '.join([MORSE_CODE.get(char, '') for char in word if char in MORSE_CODE]) 
+                           for word in text.split()])
+    
+    # Write to the destination file
+    with open(output_file, 'w', encoding='utf-8') as f_out:
+        f_out.write(morse_text)
